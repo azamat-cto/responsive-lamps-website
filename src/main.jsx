24f41220646909fb31dc94dfa262f40ab3f4a943
azamatkha6i6ullaev <azamatkha6i6ullaev@gmail.com>
2023-06-25 15:11:39 +0500

    Add swiper css

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import 'unfonts.css'
 import 'remixicon/fonts/remixicon.css'
+import 'swiper/css'
+import 'swiper/css/navigation'
 import './styles/index.scss'
 
 const root = createRoot(document.getElementById('root'))
